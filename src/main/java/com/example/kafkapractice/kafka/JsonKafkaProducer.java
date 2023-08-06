9b685e606e24ec639ae5555d071e14bd37422ec2
Andrei Shumov <staydoomed@gmail.com>
2023-08-06 19:56:00 +0300

    add Json kafka producer

---
+++ src/main/java/com/example/kafkapractice/kafka/JsonKafkaProducer.java
@@ -0,0 +1,28 @@
+package com.example.kafkapractice.kafka;
+
+import com.example.kafkapractice.model.Book;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JsonKafkaProducer {
+    private KafkaTemplate<String, Book> kafkaTemplate;
+
+    @Autowired
+    public JsonKafkaProducer(KafkaTemplate<String, Book> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    public void sendMessage(Book book){
+        log.info("Message sent: "+book.toString());
+        Message<Book> message= MessageBuilder.withPayload(book)
+                .setHeader(KafkaHeaders.TOPIC,"newTopic")
+                .build();
+        kafkaTemplate.send(message);
+    }
+}
