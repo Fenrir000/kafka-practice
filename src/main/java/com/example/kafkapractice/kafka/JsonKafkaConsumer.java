0f152c467b880aba4bbbc461c23d6b6da9043cfd
Andrei Shumov <staydoomed@gmail.com>
2023-08-06 20:28:22 +0300

    add JsonKafkaConsumer

--- src/main/java/com/example/kafkapractice/kafka/JsonKafkaConsumer.java
+++ src/main/java/com/example/kafkapractice/kafka/JsonKafkaConsumer.java
@@ -1,4 +1,16 @@
 package com.example.kafkapractice.kafka;
 
+import com.example.kafkapractice.model.Book;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class JsonKafkaConsumer {
+    @KafkaListener(topics = "newTopicJson",groupId = "myGroup")
+    public void consume(Book book){
+        log.info("Json message from producer "+ book.toString());
+
+    }
 }
