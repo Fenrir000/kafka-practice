d6bed0ac953660d95abc1f5acd6db62919bcc0b4
Andrei Shumov <staydoomed@gmail.com>
2023-08-06 19:45:47 +0300

    add Json value serializer and deserializer, and pojo Book

---
+++ src/main/java/com/example/kafkapractice/model/Book.java
@@ -0,0 +1,16 @@
+package com.example.kafkapractice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    private String title;
+    private String author;
+
+    private int year;
+
+}
